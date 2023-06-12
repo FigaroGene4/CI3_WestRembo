e690dcb216a32b72e685156182d8050a03374e01
FigaroGene4 <markgenesis37@gmail.com>
2023-06-13 00:29:49 +0800

    acccpt

--- admin/accept.php
+++ admin/accept.php
@@ -5,6 +5,23 @@
 	include_once('connection.php');
 
 	if(isset($_GET['id'])){
+		$id = $_GET['id'];
+		$sql = "SELECT * FROM table_residents WHERE id ='$id'";
+
+
+                      //use for MySQLi-OOP
+                      $query = $conn->query($sql);
+                      while ($row = $query->fetch_assoc()) {
+
+						$name = $row['firstName'] . ' '.  $row['lastName'];
+					$email = $row['email'];
+					$subject = "Successfuly registered";
+                    $message = "Hello, " .$name .". You have successfuly registered to the West Rembo App.";
+                    $sender = "From: westrembo.ph@gmail.com";
+                    
+					mail($email, $subject, $message, $sender); 
+
+					  }
 
         
         $valid = 'verified';
